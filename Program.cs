a5586afc530413c1d995e47613e3b0350f510ff8
elorasmith <eloraasmith@gmail.com>
2025-03-19 11:39:01 -0600

    Working Arrows

--- Program.cs
+++ Program.cs
@@ -11,5 +11,20 @@ if (Console.ReadKey(true).Key == ConsoleKey.Enter)
     Console.WriteLine(row);
 }
 
-
-
+ConsoleKey key;
+int x = 0;
+int y = 0;
+do
+{
+    Console.SetCursorPosition(x, y);
+    key = Console.ReadKey(false).Key;
+    if (key==ConsoleKey.LeftArrow)
+        x--;
+    else if (key==ConsoleKey.RightArrow)
+       x++;
+    else if (key==ConsoleKey.UpArrow)
+        y--;
+    else if (key==ConsoleKey.DownArrow)
+        y++;
+}
+while (key != ConsoleKey.Escape);
